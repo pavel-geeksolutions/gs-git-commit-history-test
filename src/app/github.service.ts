86d3e34ff84252185d641445704466fe1cb6ce1e
Pavel Zhernosek <pavel.zhernosek@geeksolutions.co>
2019-12-26 16:05:25 +0300

    github users search implemented

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GithubUser } from './github-user';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+const BASE_URL = 'https://api.github.com';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getUsers(term: string): Observable<GithubUser[]> {
+    return this.httpClient.get<any>(`${BASE_URL}/search/users?q=${term}`).pipe(
+      map(responseBody => {
+        return responseBody.items.map((item) => {
+          const {id, login, avatar_url, gravatar_id, score} = item;
+          return {
+            id, login, avatarUrl: avatar_url, gravatarId: gravatar_id, score
+          };
+        });
+      })
+    );
+  }
 }
