6ff9af1d8c8641c02e5d6fb94f050ecb03b81905
Pavel Zhernosek <pavel.zhernosek@geeksolutions.co>
2019-12-26 16:21:28 +0300

    github repos search implemented

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { GithubUser } from './github-user';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { GithubRepo } from './github-repo';
 
 const BASE_URL = 'https://api.github.com';
 
@@ -21,6 +22,32 @@ export class GithubService {
           const {id, login, avatar_url, gravatar_id, score} = item;
           return {
             id, login, avatarUrl: avatar_url, gravatarId: gravatar_id, score
+          };
+        });
+      })
+    );
+  }
+
+  getRepos(userLogin: string): Observable<GithubRepo[]> {
+    return this.httpClient.get<any>(
+      `${BASE_URL}/users/${userLogin}/repos`).pipe(
+      map(responseBody => {
+        return responseBody.map((item) => {
+          const {
+            id, name, description, size,
+            language
+          } = item;
+          return {
+            id,
+            name,
+            fullName: item.full_name,
+            description,
+            defaultBranch: item.default_branch,
+            size,
+            topics: item.topics || [],
+            language,
+            created: item.created_at,
+            updated: item.updated_at
           };
         });
       })
